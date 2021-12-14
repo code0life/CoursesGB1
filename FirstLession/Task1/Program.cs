40f195ccf7a3924d17c80ead5d4e56f3fc442471
code0life <34949894+code0life@users.noreply.github.com>
2021-12-14 20:28:26 +0300

    Add Task1 for Lession1
    Add Task1 for Lession1

---
+++ FirstLession/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        //Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%,
+        //сегодня %дата%». Имя пользователя сохранить из консоли в промежуточную переменную.
+
+        static void Main(string[] args)
+        {
+            string name = Environment.UserName;
+            DateTime thisDay = DateTime.Today;
+
+            string text = $"Привет, {name}, сегодня {thisDay.ToString("D")}\n";
+
+            Console.Write(text + "\nНажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
