9cb6fd9143ce6fcfbbd523dddf921bf1341ff7fa
code0life <34949894+code0life@users.noreply.github.com>
2021-12-19 00:13:50 +0300

    add task1-6 for lession2

---
+++ SecondLession/Task3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3
+{
+    //Определить, является ли введённое пользователем число чётным.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int a = GetNumber();
+            string state = (a % 2) == 0 ? "четное" : "нечетное";
+            Console.WriteLine($"\nВаше число: {a} - {state}");
+        }
+        static int GetNumber()
+        {
+            string str = Console.ReadLine();
+            int number = str.Length > 0 ? int.Parse(str) : 0;
+            return number;
+        }
+
+    }
+}
